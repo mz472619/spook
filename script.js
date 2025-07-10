c90a7ca0ece2dd02bb32f6c55d687ff43d3ba1e1
mz472619 <tnguyen2@willamette.edu>
2025-07-10 17:07:09 +0000

    eye

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function hello() {
-    alert("Hello, world!")
+function hello(event) {
+
+    const element = document.getElementById("pupil");
+
+    const x = 3 * event.pageX / window.innerWidth;
+
+    const y = 3 * event.pageY / window.innerHeight;
+
+    element.style["left"] = x + "vmin";
+    element.style["top"] = y + "vmin";
 }
+
+document.addEventListener('click', hello);
